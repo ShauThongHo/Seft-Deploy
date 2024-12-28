95d0ad1c3a137ce89305b3d660c07b9bf682a1e8
ShauThongHo <161423722+ShauThongHo@users.noreply.github.com>
2024-12-28 12:35:13 +0800

    Update youtubeDownloader.py

--- youtubeDownloader/youtubeDownloader.py
+++ youtubeDownloader/youtubeDownloader.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import yt_dlp
-import pafy
+import pytube
 import os
 import threading
 import zipfile
@@ -48,21 +48,21 @@ def download_individual_with_ytdlp(url, choice, cookies_file):
                 return None
     except yt_dlp.utils.DownloadError as e:
         st.error(f"yt-dlp error: {str(e)}")
-        return download_individual_with_pafy(url, choice)
+        return download_individual_with_pytube(url, choice)
 
-# Function to download individual video or audio using pafy
-def download_individual_with_pafy(url, choice):
+# Function to download individual video or audio using pytube2
+def download_individual_with_pytube(url, choice):
     try:
-        video = pafy.new(url)
-        best = video.getbest() if choice == 'Video' else video.getbestaudio()
-        file_name = best.download()
+        yt = pytube.YouTube(url)
+        stream = yt.streams.get_highest_resolution() if choice == 'Video' else yt.streams.filter(only_audio=True).first()
+        file_name = stream.download()
         if os.path.exists(file_name):
             return file_name
         else:
             st.error("File not found after download.")
             return None
     except Exception as e:
-        st.error(f"pafy error: {str(e)}")
+        st.error(f"pytube error: {str(e)}")
         return None
 
 # Function to download YouTube playlist using yt-dlp
